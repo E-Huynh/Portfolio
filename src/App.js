bfe7fdd42e4c0f73bce15b1c330ebbef02a3c8c7
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-07 18:51:15 -0600

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Erik</h1>
+      <Navbar />
     </div>
   );
 }
