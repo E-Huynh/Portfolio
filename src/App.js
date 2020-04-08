34519863de5d9023243e90205a01baca0cf61759
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-07 20:40:28 -0600

    revert to base react app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from './components/navbar';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        {/* <Route exact path="/" component={Home} /> */}
-      </div>
-    </Router>
+    <div>
+      <h1>Erik</h1>
+    </div>
   );
 }
 
