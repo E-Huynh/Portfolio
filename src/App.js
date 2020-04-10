f693c32053c90a1abb419d9d8101bb321f46d051
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-10 14:56:12 -0600

    Created projects page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import NavBar from './components/layout/NavBar'
+import NavBar from './components/layout/NavBar';
 import PortfolioContainer from './components/pages/portfolioContainer';
+import ProjectContainer from './components/pages/projectContainer';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <PortfolioContainer/>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        {/* <PortfolioContainer /> */}
+        <Route exact path="/" component={PortfolioContainer} />
+        <Route exact path="/projects" component={ProjectContainer} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/pages/projectContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Grid } from '@material-ui/core';
+
+
+export default function projectContainer() {
+    return (
+        <Container>
+            <Grid container>
+                <Grid item>
+                    Testing grid
+                </Grid>
+            </Grid>
+        </Container>
+    )
+}
