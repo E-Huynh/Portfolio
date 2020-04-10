99dc34f7e12db90427839a7f780980187337d022
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-09 22:57:40 -0600

    more text formatting

--- src/components/pages/portfolioContainer.js
+++ src/components/pages/portfolioContainer.js
@@ -38,27 +38,23 @@ export default function portfolioContainer() {
                     <Paper className={classes.paper}>
                         <Typography variant='h4'>
                             Biography
-                            <hr/>
+                            <hr />
                         </Typography>
                         <Typography variant='body1'>
                             Full Stack Web Developer leveraging multiple years of experience to manage, execute, and deliver products. Attained a B.S. in Biotechnology with 7+ years of research and development experience, and a certification in Full Stack Development from the University of Utah. First-hand knowledge of HTML5, CSS3, Javascript, React.js, Node and many other technologies in web development. Takes pleasure in the continual learning and collaboration amongst teams to accomplish a shared goal. Years of development experience enables me to contribute to problem solving, project management, and testing in web development.
                         </Typography>
                         <Typography variant='h4'>
                             Info
-                            <hr/>
+                            <hr />
                         </Typography>
                         <Typography variant='body1'>
-                            <ul>
-                                <li>
-                                    Phone: 801-699-3049
-                                </li>
-                                <li>
-                                    Email: Erik.W.Huynh@Gmail.com
-                                </li>
-                                <li>
-                                    Location: West Jordan, Utah
-                                </li>
-                            </ul>
+                            Phone: 801-699-3049
+                        </Typography>
+                        <Typography variant='body1'>
+                            Email: Erik.W.Huynh@Gmail.com
+                        </Typography>
+                        <Typography variant='body1'>
+                            Location: West Jordan, UT
                         </Typography>
                     </Paper>
                 </Grid>
