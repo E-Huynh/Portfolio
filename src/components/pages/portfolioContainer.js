8ab843c3318796ea7432da9b63e369b74e4c4791
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-09 22:48:10 -0600

    Basic layout of about me page

--- src/components/pages/portfolioContainer.js
+++ src/components/pages/portfolioContainer.js
@@ -7,15 +7,13 @@ const useStyles = makeStyles({
     paper: {
         padding: 20,
         color: '#747474',
-        background: '#272727'
+        background: '#272727',
+        height: '100%'
     },
     header: {
         padding: 10,
-        color: '#9407e0'
-    },
-    foo: {
-        padding: 10,
-        color: 'pink'
+        marginTop: 10,
+        color: '#9407e0',
     }
 });
 
@@ -33,13 +31,34 @@ export default function portfolioContainer() {
                 </Grid>
                 <Grid item sm={4}>
                     <Paper className={classes.paper}>
-                        <img src={bioImage} alt="Bio image" height={'100%'} width={'100%'} />
+                        <img src={bioImage} alt="Bio" height={'100%'} width={'100%'} />
                     </Paper>
                 </Grid>
                 <Grid item sm={8}>
                     <Paper className={classes.paper}>
+                        <Typography variant='h4'>
+                            Biography
+                            <hr/>
+                        </Typography>
                         <Typography variant='body1'>
                             Full Stack Web Developer leveraging multiple years of experience to manage, execute, and deliver products. Attained a B.S. in Biotechnology with 7+ years of research and development experience, and a certification in Full Stack Development from the University of Utah. First-hand knowledge of HTML5, CSS3, Javascript, React.js, Node and many other technologies in web development. Takes pleasure in the continual learning and collaboration amongst teams to accomplish a shared goal. Years of development experience enables me to contribute to problem solving, project management, and testing in web development.
+                        </Typography>
+                        <Typography variant='h4'>
+                            Info
+                            <hr/>
+                        </Typography>
+                        <Typography variant='body1'>
+                            <ul>
+                                <li>
+                                    Phone: 801-699-3049
+                                </li>
+                                <li>
+                                    Email: Erik.W.Huynh@Gmail.com
+                                </li>
+                                <li>
+                                    Location: West Jordan, Utah
+                                </li>
+                            </ul>
                         </Typography>
                     </Paper>
                 </Grid>
