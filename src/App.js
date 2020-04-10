203665a283fc6d0ebf4ac02ba52d464cccac7e8b
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-09 20:02:30 -0600

    Reset to build a skeleton layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import NavBar from './components/Navbar'
+import NavBar from './components/layout/NavBar'
+import PortfolioContainer from './components/layout/portfolioContainer';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <PortfolioContainer/>
     </div>
   );
 }
