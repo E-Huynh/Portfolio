b585566502cc75a930bbed3997de0b542dcc53a9
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-09 22:34:51 -0600

    updated new image

--- src/components/pages/portfolioContainer.js
+++ src/components/pages/portfolioContainer.js
@@ -1,21 +1,43 @@
 import React from 'react'
 import { Container, Grid, Paper, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import bioImage from '../../assets/Portfolio-Image.JPG'
+
+const useStyles = makeStyles({
+    paper: {
+        padding: 20,
+        color: '#747474',
+        background: '#272727'
+    },
+    header: {
+        padding: 10,
+        color: '#9407e0'
+    },
+    foo: {
+        padding: 10,
+        color: 'pink'
+    }
+});
 
 export default function portfolioContainer() {
 
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const classes = useStyles()
     return (
         <Container>
-            <Typography variant='h1' align='center'>
-                Portfolio container
-            </Typography>
             <Grid container spacing={2}>
+                <Grid item sm={12}>
+                    <Typography variant='h3' align='left' className={classes.header}>
+                        About Myself
+                    </Typography>
+                </Grid>
                 <Grid item sm={4}>
-                    <Paper>
-                        <img src="https://e-huynh.github.io/updated_portfolio/assets/About_me.JPG" alt="" height="100%" width="100%"/>
+                    <Paper className={classes.paper}>
+                        <img src={bioImage} alt="Bio image" height={'100%'} width={'100%'} />
                     </Paper>
                 </Grid>
                 <Grid item sm={8}>
-                    <Paper>
+                    <Paper className={classes.paper}>
                         <Typography variant='body1'>
                             Full Stack Web Developer leveraging multiple years of experience to manage, execute, and deliver products. Attained a B.S. in Biotechnology with 7+ years of research and development experience, and a certification in Full Stack Development from the University of Utah. First-hand knowledge of HTML5, CSS3, Javascript, React.js, Node and many other technologies in web development. Takes pleasure in the continual learning and collaboration amongst teams to accomplish a shared goal. Years of development experience enables me to contribute to problem solving, project management, and testing in web development.
                         </Typography>
